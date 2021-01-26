f2d78b46a13186571eb1bfbaa4d3fd80a6dc2ab6
Cristiano Caon <cristiano.caon@hotmail.com>
2021-01-26 10:02:59 -0600

    Routes, middleware and dotenv

--- myApp.js
+++ myApp.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var app = express();
+require('dotenv').config({ path: __dirname + '/.env' });
+
+app.use(express.static(__dirname + '/public'));
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  absolutePath = __dirname + '/views/index.html';
+  res.sendFile(absolutePath);
+})
+
+app.get('/json', (req, res) => {
+  let response = "Hello json";
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    response = response.toUpperCase();
+  }
+  res.json({
+    "message": response
+  });
+})
 
 
 
@@ -32,8 +51,4 @@ var app = express();
 
 
 
-
-
-
-
- module.exports = app;
+module.exports = app;
