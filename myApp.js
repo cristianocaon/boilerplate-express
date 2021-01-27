d8f988b67316b8b1d837c8345b25a93cc7ec35f3
Cristiano Caon <cristiano.caon@hotmail.com>
2021-01-26 23:26:12 -0600

    Middleware implemented

--- myApp.js
+++ myApp.js
@@ -5,6 +5,11 @@ require('dotenv').config({ path: __dirname + '/.env' });
 app.use(express.static(__dirname + '/public'));
 app.use('/assets', express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+});
+
 app.get('/', (req, res) => {
   absolutePath = __dirname + '/views/index.html';
   res.sendFile(absolutePath);
@@ -20,9 +25,16 @@ app.get('/json', (req, res) => {
   });
 })
 
+const addCurrentTime = (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}
 
-
-
+app.get('/now', addCurrentTime, (req, res) => {
+  res.json({
+    "time": req.time
+  });
+})
 
 
 
